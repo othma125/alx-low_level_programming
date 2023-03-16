824470c13e7c98efebb7c92dd1ee5bd65c21d703
othmane <othma125@hotmail.com>
2023-03-16 18:00:14 +0100

    put and printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - ENTRY POINT
+ *
+ * Return: value is success
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the output is a piece of art;\n");
+	return (0);
+}
