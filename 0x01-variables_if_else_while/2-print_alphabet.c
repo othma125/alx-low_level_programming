31936acb2e5f59e9541b45b5873554273986607d
othmane <othma125@hotmail.com>
2023-03-17 16:30:49 +0100

    print alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	do {
+		putchar(c);
+		c++;
+	} while (c <= 'z');
+	putchar('\n');
+	return (0);
+}
