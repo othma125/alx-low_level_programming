390a9863d897bf1157771885cff70cb99cbd0b22
othmane <othma125@hotmail.com>
2023-03-17 19:09:23 +0100

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,21 @@
 int main(void)
 {
 	int n = 1;
+	int comb1, comb2;
 	int max = 90;
 
 	do {
-		putchar(n / 10 + '0');
-		putchar(n % 10 + '0');
-		if (n < max - 1)
+		comb1 = n;
+		comb2 = (n % 10) * 10 + (n / 10);
+		if (comb1 < comb2)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(n / 10 + '0');
+			putchar(n % 10 + '0');
+			if (n < max - 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 		n++;
 	} while (n < max);
