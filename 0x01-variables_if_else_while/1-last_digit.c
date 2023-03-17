64d74c4b3c6c9f568b131d12e08ae559919a341a
othmane <othma125@hotmail.com>
2023-03-17 16:15:57 +0100

    last digit fourth commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,12 +9,10 @@
  */
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit;
-
 	last_digit = n % 10;
 	printf("Last digit of %d is %d and is ", n, last_digit);
 	if (last_digit == 0)
