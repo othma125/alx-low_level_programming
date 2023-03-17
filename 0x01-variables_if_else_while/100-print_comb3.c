a66ecf4a8f64ab628fe092a84bf8e2861315d27d
othmane <othma125@hotmail.com>
2023-03-17 20:23:57 +0100

    print comb3 last commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,10 @@
 int main(void)
 {
 	int n = 1;
-	int comb1, comb2;
 	int max = 90;
 
 	do {
-		comb1 = n;
-		comb2 = (n % 10) * 10 + (n / 10);
-		if (comb1 < comb2)
+		if (n / 10 < n % 10)
 		{
 			putchar(n / 10 + '0');
 			putchar(n % 10 + '0');
