221629cbf4f26f2e590111210ddaebbbe505a81a
othmane <othma125@hotmail.com>
2023-03-21 19:42:16 +0000

    print times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * print - print an integer
+ *
+ * @n: input integer
+ * Return: None
+ */
+void print(int n)
+{
+	_putchar(',');
+	if (n < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + n);
+	}
+	else if (n < 100)
+	{
+		_putchar(' ');
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		print(n / 10);
+		_putchar('0' + n % 10);
+	}
+}
+/**
+ * times_table - check the code
+ * @n: input integer
+ * Return: None.
+ */
+void times_table(int n)
+{
+	int i, j, multiplication;
+
+	if (n > 15 || n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < n ; j++)
+		{
+			multiplication = i * j;
+			if (j == 0)
+				_putchar('0');
+			else
+				print(multiplication);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_times_table(int);
 void times_table(void);
 void jack_bauer(void);
 void print_alphabet_x10(void);
 void print_alphabet(void);
-void print_to_98(int n);
+void print_to_98(int);
 int add(int, int);
-int print_sign(int n);
-void test_islower(int n);
-int _isalpha(int c);
-int _islower(int c);
+int print_sign(int);
+void test_islower(int);
+int _isalpha(int);
+int _islower(int);
 int _abs(int);
 int print_last_digit(int);
-int _putchar(char c);
+int _putchar(char);
 
 #endif
