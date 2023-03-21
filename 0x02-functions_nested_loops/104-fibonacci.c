459ea1fdb72aff74da0e158f23c8a16890f993ed
othmane <othma125@hotmail.com>
2023-03-21 22:17:52 +0000

    long fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, n = 98;
+	long double a, b, sum;
+
+	for (i = 0; i < n ; i++)
+	{
+		if (i == 0)
+		{
+			a = 0;
+			b = 1;
+			sum = 1;
+		}
+		else if (i == 1)
+		{
+			a = 1;
+			b = 2;
+			sum = 2;
+		}
+		else
+		{
+			sum = a + b;
+			a = b;
+			b = sum;
+		}
+		printf("%Lf", sum);
+		if (i != n - 1)
+			printf(", ");
+	}
+	putchar('\n');
+	return (0);
+}
