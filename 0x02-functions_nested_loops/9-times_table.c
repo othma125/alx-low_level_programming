5dce30074fef28d17e9d4e70101dda870db3899f
othmane <othma125@hotmail.com>
2023-03-21 17:43:14 +0000

    times table second commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,18 +6,24 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, multiplication;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 10 ; j++)
 		{
-			_putchar('0' + i * j);
-			if (j < 9)
+			multiplication = i * j;
+			if (multiplication == 0)
+				_putchar('0');
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+				if (multiplication / 10 == 0)
+					_putchar(' ');
+				else
+					_putchar('0' + multiplication / 10);
+				_putchar('0' + multiplication % 10);
 			}
 		}
 		_putchar('\n');
