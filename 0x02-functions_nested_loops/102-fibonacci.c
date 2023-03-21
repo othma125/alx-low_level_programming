b68293b2a650f72d95d65ceab15f40e223fa0138
othmane <othma125@hotmail.com>
2023-03-21 21:46:23 +0000

    fibonacci second commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	long long int i, a, b, sum;
+	int i;
+	long int a, b, sum;
 
 	for (i = 0; i < 50 ; i++)
 	{
@@ -29,6 +30,6 @@ int main(void)
 			a = b;
 			b = sum;
 		}
-		printf("%lld, ", sum);
+		printf("%ld, ", sum);
 	}
 }
