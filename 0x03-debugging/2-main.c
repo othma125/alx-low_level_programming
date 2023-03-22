88c655950facbe41f64b18ce9074492ad0eed8f5
othmane <othma125@hotmail.com>
2023-03-22 15:03:13 +0000

    largest number + print days

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-  int a, b, c;
-  int largest;
+	int a, b, c;
+	int largest;
 
-  a = -72;
-  b = 98;
-  c = 100;
+	a = -72;
+	b = 72;
+	c = -72;
 
-  largest = largest_number(a, b, c);
+	largest = largest_number(a, b, c);
 
-  printf("%d is the largest number\n", largest);
+	printf("%d is the largest number\n", largest);
 
-  return (0);
+	return (0);
 }
