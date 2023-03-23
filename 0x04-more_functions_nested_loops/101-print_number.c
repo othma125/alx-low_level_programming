b037d3465c6b7ee8d41e571d481cb32486d038ba
othmane <othma125@hotmail.com>
2023-03-23 15:16:07 +0000

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers
+ * @n: input number
+ * Return: None
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		print_number(-n);
+		return;
+	}
+	if (n >= 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+}
