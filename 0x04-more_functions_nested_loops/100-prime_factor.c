afab43e5a84bb42197a828f045184973c7120477
othmane <othma125@hotmail.com>
2023-03-23 14:54:07 +0000

    prime factor commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * _isprime - check if an integer is prime
+ * @n: input number
+ * Return: true or false
+ */
+int _isprime(long int n)
+{
+	int i, flag = 0;
+
+	for (i = 2; i <= n / 2; ++i)
+	{
+		if (n % i == 0)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	return (flag);
+}
+/**
+ * main - largest prime decomposition of an integer
+ * Return: successful
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	int i, j = 0;
+
+	for (i = n / 2; i >= 2 ; i--)
+	{
+		if (n % i == 0 && _isprime(i) == 1)
+		{
+			printf("%d\n", i);
+			j = i;
+			break;
+		}
+	}
+	if (j == 0)
+		printf("%ld\n", n);
+	return (0);
+}
