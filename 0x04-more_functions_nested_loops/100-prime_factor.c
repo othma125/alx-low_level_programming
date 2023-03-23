216968bdea26fdd37e2c1f55b324340413007c0e
othmane <othma125@hotmail.com>
2023-03-23 15:31:33 +0000

    folder creation fifth commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,13 +7,13 @@
  */
 int _isprime(long int n)
 {
-	int i, flag = 0;
+	int i, flag = 1;
 
-	for (i = 2; i <= n / 2; ++i)
+	for (i = 2; i <= n / 2; i++)
 	{
 		if (n % i == 0)
 		{
-			flag = 1;
+			flag = 0;
 			break;
 		}
 	}
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,6 +14,10 @@ void print_number(int n)
 		return;
 	}
 	if (n >= 10)
+	{
 		print_number(n / 10);
-	_putchar('0' + n % 10);
+		_putchar('0' + n % 10);
+	}
+	else
+		_putchar('0' + n);
 }
