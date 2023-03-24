d1fd07c989326b17b198e79187b9db561932ea16
othmane <othma125@hotmail.com>
2023-03-24 00:24:20 +0000

    print number seventh commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,15 +5,14 @@
  * @n: input number
  * Return: None
  */
-void print_number(long int n)
+void print_number(int n)
 {
 	if (n < 0)
 	{
 		_putchar('-');
 		print_number(-n);
-		return;
 	}
-	if (n >= 10)
+	else if (n >= 10)
 	{
 		print_number(n / 10);
 		_putchar('0' + n % 10);
