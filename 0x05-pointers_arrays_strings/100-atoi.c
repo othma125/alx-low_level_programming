e43d071505bb636e4affc4f732b332b56329e47c
othmane <othma125@hotmail.com>
2023-03-27 13:41:39 +0000

    atoi second commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,13 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int n = 0, negative = 0;
+	long int n = 0;
+	int negative = 0;
 
 	while (*(s + i) != '\0')
 	{
 		if (s[i] == '-')
 			negative = 1;
-		else if (s[i] == '+')
-			negative = 0;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			n *= 10;
