bc356c31bc92ef9736c453f4d2603af6d92516e8
othmane <othma125@hotmail.com>
2023-03-29 06:41:05 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - function name
+ * @src: input string
+ * @dest: output string
+ * @n: n bytes to be copy at most
+ * Return: new string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = strlen(src);
+	int i = 0;
+
+	while (i < n && i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i >= len)
+		dest[i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 char *_strcat(char *, char *);
 char *_strncat(char *, char *, int);
-
+char *_strncpy(char *, char *, int);
 
 
 
