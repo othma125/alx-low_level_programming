560ee0770c9e1ba872d414bbcb5e199358b14896
othmane <othma125@hotmail.com>
2023-03-29 06:03:22 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - function name
+ * @src: input string
+ * @dest: output string
+ * Return: new string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int n = strlen(dest);
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i + n] = src[i];
+		i++;
+	}
+	return (dest);
+}
