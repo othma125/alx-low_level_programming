a209da92e7e6f3eb57bdfee5844bc40d4571604f
othmane <othma125@hotmail.com>
2023-03-30 00:12:32 +0000

    new folder

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,6 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int);
 void _putchar(char);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-
+char *infinite_add(char *, char *, char *, int);
+void print_buffer(char *, int);
 #endif
