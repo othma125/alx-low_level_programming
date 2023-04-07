3a54a124ee1675a7eba4a7a2bd4eb931f50ae852
othmane <othma125@hotmail.com>
2023-04-07 14:53:32 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function with argc and argv
+ * @argc: parameters counter
+ * @argv: parameters names
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
