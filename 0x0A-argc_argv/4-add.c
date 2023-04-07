8da19dc155ba0d9702709d80bef091f1628a4cce
othmane <othma125@hotmail.com>
2023-04-07 15:32:25 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - main function with argc and argv
+ * @argc: parameters counter
+ * @argv: parameters names
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, converted, i = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	do {
+		converted = atoi(argv[i]);
+		if (converted == 0 && strcmp(argv[i], "0") != 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += converted;
+		i++;
+	} while (i < argc);
+	printf("%d\n", sum);
+	return (0);
+}
