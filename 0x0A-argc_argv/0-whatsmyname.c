f8ec84d07139c557ac679b1a581e591fefbac58f
othmane <othma125@hotmail.com>
2023-04-07 14:45:36 +0000

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function with argc and argv
+ * @argc: parameters counter
+ * @argv: parameters names
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
