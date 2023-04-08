b70c3398e2f8043ad21557b3801e10bc129a260b
othmane <othma125@hotmail.com>
2023-04-08 05:34:39 +0000

    change second commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,6 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
+	int values[] = {25, 10, 5, 2, 1}, *v = values;
 
 	if (argc != 2)
 	{
@@ -20,31 +21,9 @@ int main(int argc, char *argv[])
 	cents = atoi(argv[1]);
 	while (cents > 0)
 	{
-		if ((cents - 25) >= 0)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if ((cents - 10) >= 0)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if ((cents - 5) >= 0)
-		{
-			cents -= 5;
-			coins++;
-		}
-		else if ((cents - 2) >= 0)
-		{
-			cents -= 2;
-			coins++;
-		}
-		else
-		{
-		    cents--;
-		    coins++;
-		}
+		coins += cents / *v;
+		cents = cents % *v;
+		v++;
 	}
 	printf("%d\n", coins);
 	return (0);
