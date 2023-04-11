f7f38872d77515a442820ba2dcd5f0166afe4652
othmane <othma125@hotmail.com>
2023-04-11 06:46:36 +0000

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - check the code
+ * @str: stringr
+ * Return: array pointer
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	new_str = (char *)malloc(sizeof(char) * strlen(str));
+	if (new_str == NULL)
+		return (NULL);
+	for (i = 0; i < strlen(str); i++)
+		*(new_str + i) = *(str + i);
+	return (new_str);
+}
