6e5fd3e6db064d95cbc32b1f610b15cf9337ae41
othmane <othma125@hotmail.com>
2023-04-17 13:43:57 +0000

    init dog second commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "dog.h"
+
 /**
  * init_dog - check code
  *@d: dog pointer
@@ -10,9 +10,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
