15d88be4c8c265cb7b360fe3c059ea9e87cb2360
othmane <othma125@hotmail.com>
2023-04-17 13:58:57 +0000

    print dog third commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", d->name);
+	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
 	printf("Age: %06f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
