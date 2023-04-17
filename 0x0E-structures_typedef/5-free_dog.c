3a441b76085cece0048b54e7992ae0f2d55c8d9c
othmane <othma125@hotmail.com>
2023-04-17 14:56:10 +0000

    free dog second commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdio.h>
+
 /**
  * free_dog - check code
  *@d: dog pointer
@@ -9,9 +9,7 @@ void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name != NULL)
-		free(d->name);
-	if (d->owner != NULL)
-		free(d->owner);
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
