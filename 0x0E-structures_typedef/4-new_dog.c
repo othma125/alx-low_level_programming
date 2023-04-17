f84634244943c927ee134b194de35f1d7d252275
othmane <othma125@hotmail.com>
2023-04-17 14:31:04 +0000

    new dog commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,56 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *_strlen - returns length of a string
+ *@str: string
+ *Return: integer
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+
+/**
+ *clone - clone a string
+ *@src: string
+ *Return: returns copied string
+ */
+char *clone(char *src)
+{
+	int i;
+	char *dest;
+
+	dest = (char *)malloc(_strlen(src) + 1);
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i] != '\0' ; index++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * new_dog - check code
+ *@name: name
+ *@age: age
+ *@owner: owner
+ *Return: dog pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	d = (dog_t *)malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	d->name = clone(name);
+	if (d->name == NULL)
+		return (NULL);
+	d->age = age;
+	d->owner = clone(owner);
+	if (d->owner == NULL)
+		return (NULL);
+	return (d);
+}
