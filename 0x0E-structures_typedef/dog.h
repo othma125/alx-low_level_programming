6ac0b053b4fd0c0e96fa2092127146127d595bef
othmane <othma125@hotmail.com>
2023-04-17 13:32:16 +0000

    dog file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: check code
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
