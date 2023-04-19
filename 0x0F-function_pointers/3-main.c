58c46f3a19a955ae071e7f7546fe214fbcaf488a
othmane <othma125@hotmail.com>
2023-04-19 16:11:33 +0000

    calc second commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,19 +15,19 @@ int main(int argc, char *argv[])
 	char *op;
 
 	if (argc != 4)
-		return (1);
+		exit(98);
 	a = atoi(argv[1]);
 	op = argv[2];
 	b = atoi(argv[3]);
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		return (1);
+		exit(98);
 	}
 	if ((*op == '/' || *op == '%') && b == 0)
 	{
 		printf("Error\n");
-		return (1);
+		exit(98);
 	}
 	printf("%d\n", get_op_func(op)(a, b));
 	return (0);
