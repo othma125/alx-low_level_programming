80b1cab981164e501803ef547f973619450193f5
othmane <othma125@hotmail.com>
2023-04-19 14:54:32 +0000

    int index commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * int_index - check code
+ * @a: array
+ * @size: size of array
+ * @f: function pointer
+ * Return: integer
+ */
+int int_index(int *a, int size, int (*f)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (f(a[i]) != 0)
+			return (i);
+	return (-1);
+}
