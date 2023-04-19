5dd39c4cbeedb95dd700a5fe8ac0a4b5f977b68f
othmane <othma125@hotmail.com>
2023-04-19 16:01:04 +0000

    calc commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *op;
+
+	if (argc != 4)
+		return (1);
+	a = atoi(argv[1]);
+	op = argv[2];
+	b = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if ((*op == '/' || *op == '%') && b == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+
+/**
+ *op_add - check code
+ *@a:the first number
+ *@b:the second number
+ *
+ *Return:The sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ *op_sub - check code
+ *@a:The first number
+ *@b:The second number
+ *
+ *Return: The Difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - check code.
+ *@a:The first number
+ *@b: The second number
+ *
+ *Return: The multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ *op_div - check code.
+ *@a: The first number.
+ *@b: The second number.
+ *
+ *Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - check code.
+ *@a: The first number.
+ *@b: The second number.
+ *
+ *Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
