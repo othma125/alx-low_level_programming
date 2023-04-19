fc558a0a4b3b3dacd993977551cd97ab7e1a81b3
othmane <othma125@hotmail.com>
2023-04-19 14:52:57 +0000

    print name fourth commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
+#include <stddef.h>
 void print_name(char *, void (*f)(char *));
 void array_iterator(int *, size_t, void (*action)(int));
 int int_index(int *, int, int (*cmp)(int));
