d74dafd96ef21a577ae30b5c5f3146a9dc8196b1
othmane <othma125@hotmail.com>
2023-04-20 13:44:55 +0000

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - check code
+ * @n: constant integer
+ * Return: integer
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i;
+	va_list ap;
+
+	va_start(ap, n);
+	i = 0;
+	while (i < n)
+	{
+		sum += va_arg(ap, int);
+		i++;
+	}
+	va_end(ap);
+	return (sum);
+}
