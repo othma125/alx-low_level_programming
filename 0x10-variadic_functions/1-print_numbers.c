e9407d30a4e0cc171160c959827a3e3f1bb12ccb
othmane <othma125@hotmail.com>
2023-04-20 14:13:52 +0000

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - check code
+ * @separator: string
+ * @n: constant integer
+ * Return: integer
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", va_arg(ap, int));
+		i++;
+		if (i < n && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef VAR_FUNC_H
 #define VAR_FUNC_H
-int sum_them_all(const unsigned int, ...);
 
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
 
 
 
