b5b92c59a4fa9ec31118ef704232ffa36dc11b56
othmane <othma125@hotmail.com>
2023-04-28 15:12:34 +0000

    free list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,5 +9,7 @@ size_t listint_len(const listint_t *h)
 {
 	if (h == NULL)
 		return (0);
+	if (h->next == NULL)
+		return (1);
 	return (1 + listint_len(h->next));
 }
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - check the code
+ * @h: listheader
+ * Return: none
+ */
+void free_listint(listint_t *h)
+{
+	listint_t *node;
+
+	if (h->next != NULL)
+	{
+		node = h->next;
+		free(h);
+		free_listint(node);
+		return;
+	}
+	free(h);
+}
