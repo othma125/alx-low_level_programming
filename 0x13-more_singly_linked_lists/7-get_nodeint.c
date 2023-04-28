141aa5458f3c49a878b575dc400a131a0efb8329
othmane <othma125@hotmail.com>
2023-04-28 15:54:41 +0000

    get node index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - check the code
+ * @h: listheader
+ * @index: targeted index
+ * Return: list node
+ */
+listint_t *get_nodeint_at_index(listint_t *h, unsigned int index)
+{
+	if (h == NULL)
+		return (NULL);
+	if (index == 0)
+		return (h);
+	return (get_nodeint_at_index(h->next, index - 1));
+}
