e2871df43085a772e6a9fda2e77da99c92b7661c
othmane <othma125@hotmail.com>
2023-04-28 16:00:00 +0000

    sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_listint - check the code
+ * @h: list head
+ * Return: list sum
+ */
+int sum_listint(listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	return (h->n + sum_listint(h->next));
+}
