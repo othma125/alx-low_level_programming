7236395ecbb0a96828c1377610cd4d2a13531972
othmane <othma125@hotmail.com>
2023-04-29 17:15:22 +0000

    find loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * find_listint_loop - check the code
+ * @h: list head
+ * Return: list pointer
+ */
+listint_t *find_listint_loop(listint_t *h)
+{
+	listint_t *prv = NULL;
+
+	if (h == NULL)
+		return (NULL);
+	while (h != NULL)
+	{
+		if (prv != NULL && (unsigned long)h > (unsigned long)prv)
+		{
+			printf("-> [%p] %d\n", (void *)h, h->n);
+			return (h);
+		}
+		printf("[%p] %d\n", (void *)h, h->n);
+		prv = prv == NULL ? h : prv->next;
+		h = h->next;
+	}
+	return (NULL);
+}
