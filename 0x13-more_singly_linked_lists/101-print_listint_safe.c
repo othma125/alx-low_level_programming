c1add9859ff521e6cf66b97863baefe4fa3e87c1
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-04-29 21:15:01 +0000

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,21 +9,18 @@
  */
 size_t print_listint_safe(const listint_t *h)
 {
-	const listint_t *prv = NULL;
 	size_t count = 0;
 
 	if (h == NULL)
 		return (0);
 	while (h != NULL)
 	{
-		if ((prv != NULL && (unsigned long)h >= (unsigned long)prv)
-		    || (prv == NULL && h == h->next))
+		if (h >= h->next)
 		{
 			printf("-> [%p] %d\n", (void *)h, h->n);
 			exit(98);
 		}
 		printf("[%p] %d\n", (void *)h, h->n);
-		prv = prv == NULL ? h : prv->next;
 		h = h->next;
 		count++;
 	}
