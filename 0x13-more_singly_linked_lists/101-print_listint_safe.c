88e985457f974d9038f2a2dbba310e012c353f05
othmane <othma125@hotmail.com>
2023-04-29 22:49:08 +0000

    print list safe seventh commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,7 +18,7 @@ size_t print_listint_safe(const listint_t *h)
 		if (h <= h->next)
 		{
 			printf("-> [%p] %d\n", (void *)h, h->n);
-			exit(98);
+			break;
 		}
 		printf("[%p] %d\n", (void *)h, h->n);
 		h = h->next;
