706420049d24ef479715915f5230f53367af4795
othmane <othma125@hotmail.com>
2023-04-29 21:56:19 +0000

    print list safe fouth commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,17 +10,20 @@
 size_t print_listint_safe(const listint_t *h)
 {
 	size_t count = 0;
+	const listint_t *prv = NULL;
 
 	if (h == NULL)
 		return (0);
 	while (h != NULL)
 	{
-		printf("[%p] %d\n", (void *)h, h->n);
-		if (h >= h->next)
+		if ((prv != NULL && h >= prv)
+		    || (prv == NULL && h == h->next))
 		{
 			printf("-> [%p] %d\n", (void *)h, h->n);
 			exit(98);
 		}
+		printf("[%p] %d\n", (void *)h, h->n);
+		prv = prv == NULL ? h : prv->next;
 		h = h->next;
 		count++;
 	}
