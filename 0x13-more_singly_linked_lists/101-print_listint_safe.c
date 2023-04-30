472ba6ecc31106ef549f599181a08baf8aabb775
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-04-30 13:20:13 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_listint_safe - check the code
@@ -11,8 +10,6 @@ size_t print_listint_safe(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (!h)
-		exit(98);
 	while (h)
 	{
 		count++;
