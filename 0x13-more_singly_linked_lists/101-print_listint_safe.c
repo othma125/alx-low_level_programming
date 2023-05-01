567feddf6fb685aa41942cbe4530bfb54c671132
othmane <othma125@hotmail.com>
2023-05-01 19:37:59 +0100

    print list floyds algorithm implementation

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,25 +1,44 @@
 #include "lists.h"
 #include <stdio.h>
+#include <limits.h>
 
-#define abs(x) ((x) < 0 ? ((x) * -1) : (x))
 /**
  * detect_cycle - check the code
- * @mbs: blocks array
- * @node: list head
- * @m: blocks count
- * Return: boolean
+ * @h: list head
+ * Return: list length
  */
-int detect_cycle(memory_block *mbs, const listint_t *node, int m)
+size_t detect_cycle(const listint_t *h)
 {
-	int i;
+	const listint_t *slow, *fast;
+	size_t count = 1;
 
-	for (i = 0; i <= m; i++)
+	if (h == NULL || h->next == NULL)
+		return (0);
+	slow = h->next;
+	fast = slow->next;
+	while (fast)
 	{
-		if ((mbs[i].index <= 0 && node - mbs[i].ptr <= -2 * mbs[i].index)
-		    || (mbs[i].index >= 0 && mbs[i].ptr - node <= 2 * mbs[i].index))
-			return (i);
+		if (slow == fast)
+		{
+			slow = h;
+			while (slow != fast)
+			{
+				count++;
+				slow = slow->next;
+				fast = fast->next;
+			}
+			slow = slow->next;
+			while (slow != fast)
+			{
+				count++;
+				slow = slow->next;
+			}
+			return (count);
+		}
+		slow = slow->next;
+		fast = (fast->next)->next;
 	}
-	return (-1);
+	return (0);
 }
 /**
  * print_listint_safe - check the code
@@ -28,36 +47,15 @@ int detect_cycle(memory_block *mbs, const listint_t *node, int m)
  */
 size_t print_listint_safe(const listint_t *h)
 {
-	size_t count = 0;
-	int m = 0;
-	long dist;
-	memory_block mbs[10];
+	size_t cycle = detect_cycle(h), count = 0;
 
-	if (!h)
-		return (count);
-	mbs[m].index = 0;
-	mbs[m].ptr = h;
-	while (h)
+	while (h && count < (cycle == 0 ? INT_MAX : cycle))
 	{
 		count++;
 		printf("[%p] %d\n", (void *)h, h->n);
-		dist = h - h->next;
 		h = h->next;
-		if (detect_cycle(mbs, h, m) > 0)
-		{
-			printf("-> [%p] %d\n", (void *)h, h->n);
-			break;
-		}
-		if (dist == 2 && mbs[m].index >= 0)
-			mbs[m].index++;
-		else if (dist == -2 && mbs[m].index <= 0)
-			mbs[m].index--;
-		else
-		{
-			m++;
-			mbs[m].index = 0;
-			mbs[m].ptr = h;
-		}
 	}
+	if (cycle > 0)
+		printf("-> [%p] %d\n", (void *)h, h->n);
 	return (count);
 }
