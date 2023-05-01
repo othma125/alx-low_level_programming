2cdb6757d5c702671aa66ad0f3559fc1538244b8
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-05-01 16:15:01 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -15,10 +15,7 @@ listint_t *find_listint_loop(listint_t *h)
 	while (h != NULL)
 	{
 		if (h->next == loop_node)
-		{
-			printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
 			return (loop_node);
-		}
 		loop_node = loop_node == NULL && h->n == (h->next)->n + 1 ? h : loop_node;
 		h = h->next;
 	}
