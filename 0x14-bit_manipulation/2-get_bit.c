d04671cf1087cd8f8d89e5884b34193687d2f15a
othmane <othma125@hotmail.com>
2023-05-04 17:04:26 +0100

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - check the code
+ * @n: integer to print
+ * @i: index
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int i)
+{
+	if (i > 63)
+		return (-1);
+	if (i == 0)
+		return (n & 1);
+	get_bit(n >> 1, i - 1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,6 @@
 unsigned int binary_to_uint(const char *);
 int _putchar(char);
 void print_binary(unsigned long int);
-
+int get_bit(unsigned long int, unsigned int);
 
 #endif
