f1f9000e300a17499d1b1f7d4957618aaa0cb22b
othmane <othma125@hotmail.com>
2023-05-04 17:56:45 +0100

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - check the code
+ * @n: integer to modifiy
+ * @i: index
+ *
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int i)
+{
+	unsigned long int k;
+	unsigned int j = 0;
+
+	if (i > 63 || (*n >> i) & 1)
+		return (-1);
+	k = 1;
+	for (; j < i; j++)
+		k *= 2;
+	*n += k;
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,7 @@ unsigned int binary_to_uint(const char *);
 int _putchar(char);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
+int set_bit(unsigned long int *, unsigned int);
+
 
 #endif
