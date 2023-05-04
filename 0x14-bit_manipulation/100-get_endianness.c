bf21e58b7398da9add8ee3100bcac6b077769efb
othmane <othma125@hotmail.com>
2023-05-04 18:23:42 +0100

    password

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * get_endianness - check code
+ *
+ * Return: 1or 0
+ */
+
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *s = (char *)&test;
+
+	return (*s != '\0' ? 1 : 0);
+}
