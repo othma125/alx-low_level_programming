9db73814c444048145aa22a124cca73e5b551f02
othmane <othma125@hotmail.com>
2023-06-15 16:04:07 +0100

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,9 +38,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int i, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	tmp->prev->next = new_node;
 	new_node->next = tmp;
 	new_node->prev = tmp->prev;
-	tmp->prev->next = new_node;
 	tmp->prev = new_node;
         return (new_node);
 }
