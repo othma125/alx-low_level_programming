b0319ecd41bb3bcecd6c0bf085d373acca6323a9
othmane <othma125@hotmail.com>
2023-06-15 16:01:14 +0100

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,16 @@
 #include "lists.h"
+
+/**
+ * len - check the code
+ * @h: list head
+ * Return: list size
+ */
+unsigned int len(dlistint_t *h)
+{
+	if (!h)
+		return (0);
+	return (1 + len(h->next));
+}
 /**
  * insert_dnodeint_at_index - check the code
  * @h: list head
@@ -9,28 +21,26 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int i, int n)
 {
 	dlistint_t *new_node, *tmp = *h;
+	unsigned int length = len(*h), j = 0;
 
-	do {
-		if (tmp == NULL && i > 0)
-			return (NULL);
-		if (i == 0)
-		{
-			new_node = malloc(sizeof(dlistint_t));
-			if (new_node == NULL)
-				return (NULL);
-			new_node->n = n;
-			new_node->next = tmp;
-			new_node->prev = tmp->prev;
-			if (tmp->prev != NULL)
-			{
-				tmp->prev->next = new_node;
-				tmp->prev = new_node;
-			}
-			else
-				*h = new_node;
-			return (new_node);
-		}
+	if (length < i)
+		return (NULL);
+	if (i == 0)
+		new_node = add_dnodeint(h, n);
+	else if (i == length)
+		new_node = add_dnodeint_end(h, n);
+	while (i != j)
+	{
 		tmp = tmp->next;
-		i--;
-	} while (1);
+		j++;
+	}
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = tmp;
+	new_node->prev = tmp->prev;
+	tmp->prev->next = new_node;
+	tmp->prev = new_node;
+        return (new_node);
 }
