2173e8f8cce21150c18b4d88b30e41d7027e787d
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-19 14:47:04 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/othma125/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD =/tmp/nrandom.so
+curl -s -o ~/rand.so https://raw.githubusercontent.com/othma125/alx-low_level_programming/main/0x18-dynamic_libraries/rando.so
+export LD_PRELOAD =~/nrandom.so
