f7debbd4919a78dc7ef5dad2af7ad4c2b9313e19
othmane <othma125@hotmail.com>
2023-07-06 07:17:55 +0100

    2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - check the code
+ * @key: string
+ * @size: array size
+ * Return: key index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -33,6 +33,6 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int);
-
-
+unsigned long int hash_djb2(const unsigned char *);
+unsigned long int key_index(const unsigned char *, unsigned long int);
 #endif
