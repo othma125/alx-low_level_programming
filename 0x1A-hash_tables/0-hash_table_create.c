edbd45169a0f41a73f0e58197ac0861d770a2367
othmane <othma125@hotmail.com>
2023-07-06 07:04:09 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,13 +7,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
-    if (!ht)
-        return (NULL);
-    ht->size = size;
-    ht->array = malloc(size * sizeof(hash_node_t*));
-    if (!ht->array)
-        return (NULL);
-    return (ht);
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_t *));
+	if (!ht->array)
+		return (NULL);
+	return (ht);
 }
