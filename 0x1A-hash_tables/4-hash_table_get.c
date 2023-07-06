0e196c16b27b12a99cd03e3aa9876197de27e9cf
othmane <othma125@hotmail.com>
2023-07-06 13:32:21 +0100

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - check the code
+ * @ht: hash table
+ * @key: string key
+ * Return: value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+  
+}
