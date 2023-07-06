2d87e3edeecde3136b228232cf0b2a1a8e6326f3
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-06 06:51:02 +0100

    .\0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - check the code
+ * @size: array size
+ * Return: hash table ptr
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+    if (!ht)
+        return (NULL);
+    ht->size = size;
+    ht->array = malloc(size * sizeof(hash_node_t*));
+    if (!ht->array)
+        return (NULL);
+    return (ht);
+}
