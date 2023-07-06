883a1b79500a5c2853d6519b5ef45c6669094e57
othmane <othma125@hotmail.com>
2023-07-06 14:24:02 +0100

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,7 +8,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    if (ht == NULL || key == NULL || *key == '\0')
-    return (NULL);
-  return (NULL);
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	if (!tmp)
+		return (NULL);
+	while (tmp->next && strcmp(key, tmp->key) != 0)
+		tmp = tmp->next;
+	if (strcmp(key, tmp->key) == 0)
+		return (tmp->value);
+	return (NULL);
 }
