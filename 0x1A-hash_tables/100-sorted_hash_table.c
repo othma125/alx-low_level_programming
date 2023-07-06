d888d1c3f7f897e0998ae53d5994e62358c08dcf
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-07-06 19:58:33 +0100

    Update 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -22,3 +22,26 @@ shash_table_t *shash_table_create(unsigned long int size)
 	ht->stail = NULL;
 	return (ht);
 }
+/**
+ * shash_table_get - check the code
+ * @ht: hash table
+ * @key: string key
+ * Return: value
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	shash_node_t *tmp;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	if (!tmp)
+		return (NULL);
+	while (tmp->next && strcmp(key, tmp->key) != 0)
+		tmp = tmp->next;
+	if (strcmp(key, tmp->key) == 0)
+		return (tmp->value);
+	return (NULL);
+}
