ce2af3e5cb5ee91d307ddc46582d338b35c62fb1
othmane <othma125@hotmail.com>
2023-07-06 12:54:24 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
 	if (!ht)
 		return (NULL);
@@ -15,5 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (!ht->array)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 	return (ht);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - check the code
+ * @ht: hash table
+ * @key: string key
+ * @value: string value
+ * Return: success
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+        new = malloc(sizeof(hash_node_t *));
+	if (!new)
+		return (0);
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = strdup(value);
+	if (!new->value)
+	{
+		free(new);
+		return (0);
+	}
+	new->next = NULL;
+	index = key_index((unsigned char *)key, ht->size);
+	if (!ht->array[index])
+		ht->array[index] = new;
+	else
+	{
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
+	return (1);
+}
