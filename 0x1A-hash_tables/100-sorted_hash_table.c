167a54c8c4294326e80f0870c2c4434dd4f3c171
othmane <othma125@hotmail.com>
2023-07-07 15:56:43 +0100

    100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -68,3 +68,97 @@ void shash_table_delete(shash_table_t *ht)
 	free(ht->array);
 	free(ht);
 }
+/**
+ * shash_table_set - check the code
+ * @ht: hash table
+ * @key: string key
+ * @value: string value
+ * Return: success
+ */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	shash_node_t *new;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	new = ht->array[index];
+	while (new)
+	{
+		if (!strcmp(key, new->key))
+		{
+			free(new->value);
+			new->value = strdup(value);
+			return (1);
+		}
+		new = new->next;
+	}
+	new = malloc(sizeof(shash_node_t *));
+	if (!new)
+		return (0);
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = strdup(value);
+	if (!new->value)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
+/**
+ * shash_table_print - check the code
+ * @ht: hash table
+ * Return: nothing
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *tmp;
+	int c = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	tmp = ht->shead;
+	while (tmp)
+	{
+		if (c)
+			printf(", ");
+		printf("'%s': '%s'", tmp->key, tmp->value);
+		tmp = tmp->snext;
+		c = 1;
+	}
+	printf("}\n");
+}
+/**
+ * shash_table_print_rev - check the code
+ * @ht: hash table
+ * Return: nothing
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *tmp;
+	int c = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	tmp = ht->stail;
+	while (tmp)
+	{
+		if (c)
+			printf(", ");
+		printf("'%s': '%s'", tmp->key, tmp->value);
+		tmp = tmp->sprev;
+		c = 1;
+	}
+	printf("}\n");
+}
