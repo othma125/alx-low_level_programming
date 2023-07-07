920ea74c3a194a4c0ac5ce0e3cff9c26a975d6e2
othmane <othma125@hotmail.com>
2023-07-07 17:51:52 +0100

    100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -133,6 +133,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
+	printf("%lu\n", strlen(new->key));
 	new->value = strdup(value);
 	if (!new->value)
 	{
