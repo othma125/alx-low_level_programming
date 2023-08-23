c4066c60f9c0a6008a0068bf85f05153567bbb67
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-08-23 12:16:03 +0100

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for island_perimeter"""
+
+
+def island_perimeter(grid):
+    """Function to calculate an island described in a grid perimeter"""
+    edges = 0
+    size = 0
+    for i, row in enumerate(grid):
+        for j, element in enumerate(row):
+            if element == 1:
+                size += 1
+                if (j > 0 and row[j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
