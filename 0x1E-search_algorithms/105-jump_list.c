cf9a6e517bf06e4cc0e71f68aeb770cc2e70ff48
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-10-11 18:06:34 +0100

    Update 105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -30,7 +30,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", i - k, node->index);
-	while (tmp->n < value)
+	while (tmp && tmp->n < value)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", tmp->index, tmp->n);
 		tmp = tmp->next;
